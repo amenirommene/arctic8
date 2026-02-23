c10d827b018f8c898ba1da22ec5c8db832a85526
amenir88 <ameni.rommene@esprit.tn>
2026-02-23 13:16:27 +0100

    suite reactive Form

--- src/app/features/suggestions/form-suggestion/form-suggestion.component.ts
+++ src/app/features/suggestions/form-suggestion/form-suggestion.component.ts
@@ -9,13 +9,33 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class FormSuggestionComponent {
 
   myForm : FormGroup;
+  onSubmit(){
+
+  }
   ngOnInit(){
     this.myForm=new FormGroup({
-      title : new FormControl("",
-        [Validators.required,Validators.minLength(3)]),
-      description: new FormControl("",Validators.required)
+title : new FormControl("",
+[Validators.required,Validators.minLength(10),Validators.pattern("^[A-Z][a-zA-Z]*$")]),
+description: new FormControl("",[Validators.required,Validators.minLength(30)]),
+ category: new FormControl("",[Validators.required]),
+ status: new FormControl("en attente"),
+date:new FormControl(new Date().toISOString().split("T")[0])
     })
   }
+
+  get title(){
+    return this.myForm.get('title');
+  }
+  id: number = 0;
+
+  categories: string[] =
+[ 'Infrastructure et bâtiments',
+  'Technologie et services numériques',
+   'Restauration et cafétéria',
+   'Hygiène et environnement',
+   'Transport et mobilité',
+   'Activités et événements',
+   'Sécurité', 'Communication interne', 'Accessibilité', 'Autre' ];
 
 
 }
